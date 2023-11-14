98eb227edd0425885e619067c351937c96bfd2d2
Phạm Ngọc Trường Sơn <sonpnts@ou.edu.vn>
2023-11-14 13:01:21 +0700

    login

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask (__name__)
 app.secret_key='ahsbfhjkagsfugwgf9w7r26826r32'
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://sonpnts:% s@localhost/saleapp?charset=utf8mb4" % quote('Son1010@')
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:% s@localhost/saleapp?charset=utf8mb4" % quote('Admin@123')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 db = SQLAlchemy(app)
