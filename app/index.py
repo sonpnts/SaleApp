fe023658bc67010faebd3846465935f3cd10407c
admin <sonpnts@gmail.com>
2023-12-19 14:54:04 +0700

    update 19/12/2023

--- app/index.py
+++ app/index.py
@@ -52,6 +52,29 @@ def add_cart():
     return jsonify(utils.count_cart(cart))
 
 
+@app.route('/api/cart/<product_id>', methods=['put'])
+def update_cart(product_id):
+    cart = session.get('cart')
+    if cart and product_id in cart:
+        quantity = request.json.get('quantity')
+        cart[product_id]['quantity'] = int(quantity)
+
+    session['cart']=cart
+
+    return jsonify(utils.count_cart(cart))
+
+
+@app.route('/api/cart/<product_id>', methods=['delete'])
+def delete_cart(product_id):
+    cart = session.get('cart')
+    if cart and product_id in cart:
+        del cart[product_id]
+
+    session['cart']=cart
+
+    return jsonify(utils.count_cart(cart))
+
+
 @app.route('/admin/login', methods=['post'])
 def login_admin_process():
     username = request.form.get('username')
--- app/static/js/cart.js
+++ app/static/js/cart.js
@@ -13,9 +13,47 @@ function addToCart(id,name,price){
         return res.json();
 
     }).then(function(data){
-        console.info(data)
         let c = document.getElementsByClassName('cart-counter');
         for( let d of c)
             d.innerText = data.total_quantity
     })
+}
+
+function updateCart(id, obj){
+    obj.disabled=true;
+    fetch(`/api/cart/${id}`,{
+        method:'put',
+        body:JSON.stringify({
+            'quantity' : obj.value
+        }),
+        headers: {
+            'Content-Type':"application/json"
+        }
+    }).then(res=>res.json()).then(data=>{
+    obj.disabled = false;
+      let c = document.getElementsByClassName('cart-counter');
+      for( let d of c)
+        d.innerText = data.total_quantity
+
+      let a = document.getElementsByClassName('cart-total');
+      for( let d of a)
+        d.innerText = data.total_amount
+    })
+}
+
+function deleteCart(id, obj){
+    if (confirm("Ban chac chan muon xoa khong") ===true){
+        obj.disabled=true;
+        fetch(`/api/cart/${id}`,{
+            method:'delete',
+        }).then(res=>res.json()).then(data=>{
+        obj.disabled = false;
+          let c = document.getElementsByClassName('cart-counter');
+            for( let d of c)
+                d.innerText = data.total_quantity
+
+            let r = document.getElementById(`product${id}`);
+            r.style.display = "none"
+        })
+    }
 }
